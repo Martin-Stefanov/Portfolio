ce648224050a0f2475d956a4203d1a8f87b88a14
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-05-08 17:28:15 +0100

    React Routing - added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import NavBar from './components/NavBar.tsx';
 import VideoBackground from './components/VideoBackground.tsx';
+import HomePage from './pages/HomePage.tsx';
 
 function App() {
 
   return (
-    <div className=' bg-palms bg-cover min-h-screen w-full'>
-      <NavBar />
-      {/* <VideoBackground /> */}
-      <div className='flex justify-end items-center'>
-        <div id='buttons-box' className='flex flex-col justify-center space-y-10 w-96 h-128 border-black border-2 rounded-md mr-20 mt-10'>
+    <>
+      <BrowserRouter>
 
-          <button className='main-button'>SKILLS</button>
-          <button className='main-button'>PROJECTS</button>
-          <button className='main-button'>Where I come from</button>
-          <button className='main-button'>My Free Time</button>
-        </div>
-      </div>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/projects'/>
+        </Routes>
+      </BrowserRouter>
+      
+    </>
 
-
-    </div>
   )
 }
 
