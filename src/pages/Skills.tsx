4d30fe1372990ee619d38db5f93d76e804639b39
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-05-13 19:03:36 +0100

    Skills.tsx - added

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from "react";
 
 const Skills = () => {
 
+  const [logos, setLogos] = useState(['js-logo.jpg', 'ts-logo.png', 'html-logo.png']);
+  const [displayedLogos, setDisplayedLogos] = useState<string[]>([]);
+
+  const delay = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration));
+
+  useEffect(() => {
+    const displayLogos = async () => {
+      for (const logo of logos) {
+        if (logo !== logos[0]) {
+          await delay(2000);
+        }
+        setDisplayedLogos((prevLogos) => [...prevLogos, logo]);
+      }
+    };
+
+    displayLogos();
+  }, [logos]);
+
+
+  const removeDuplicates = (logosArray: string[]): string[] => {
+    const logosSet = new Set(logosArray);
+    return [...logosSet];
+  }
+
   return (
     <div>
-      
+      {Boolean(displayedLogos.length) && removeDuplicates(displayedLogos).map((logo) => (
+        <div className="inline-block">
+          <img
+            key={logo}
+            className='single-logo'
+            src={logo}
+            alt="TEST"
+          />
+        </div>
+
+      ))}
     </div>
-  )
+  );
+
 
 }
 export default Skills;
