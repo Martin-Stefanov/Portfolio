e4917fcfd2fd0d7d79d5c79db6ca8d79c2039f52
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-05-15 15:51:22 +0100

    GASP library - added

--- src/pages/Vinyl.tsx
+++ src/pages/Vinyl.tsx
@@ -1,10 +1,28 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
+
+gsap.registerPlugin(useGSAP);
+// CustomEase.create("cubic", "0.83, 0, 0.17, 1");
 
 const Vinyl = () => {
+  const album = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(() => {
+
+    gsap.to(album.current, { 
+     rotation: '+=360',
+     duration: 3,
+     
+     ease: 'none'
+    })
+  })
 
   return (
     <div className="relative w-[100vw] h-[89vh] overflow-hidden bg-red-400">
       <div className="slider absolute top-[15vh] w-[100vw] h-[100vh] overflow-hidden bg-purple-500">
-        <div className="album">
+        <div ref={album} className="album">
           <img className="w-[100%] h-[100%] object-cover" src="/postmodern.jpg" alt="Postmodern Jukebox Cover" />
           <div className="copy">
             <h1>Lorem, ipsumn.</h1>
