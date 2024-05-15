4d051bb4dfc0ab06554c29af31e23b7456a4393d
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-05-15 16:44:15 +0100

    uuid - installed + Vinyl.tsx - animation with GASP started

--- src/pages/Vinyl.tsx
+++ src/pages/Vinyl.tsx
@@ -1,57 +1,56 @@
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { CustomEase } from "gsap/all";
+import { v4 } from "uuid";
 
 
 gsap.registerPlugin(useGSAP);
-// CustomEase.create("cubic", "0.83, 0, 0.17, 1");
+gsap.registerPlugin(CustomEase);
+CustomEase.create("cubic", "0.83, 0, 0.17, 1");
+
 
 const Vinyl = () => {
-  const album = useRef<HTMLDivElement | null>(null);
+  const [isAnimating, setIsAnimating] = useState(false);
 
-  useGSAP(() => {
+  // const album = useRef<HTMLDivElement | null>(null);
+  const albumRefs = Array(5).fill(0).map(_ => React.createRef<HTMLDivElement>());
 
-    gsap.to(album.current, { 
-     rotation: '+=360',
-     duration: 3,
-     
-     ease: 'none'
-    })
+const initializeAlbums = (): void => {
+  gsap.to(albumRefs.map(ref => ref.current), {
+    y: (i) => -15 + 15 * i + "%",
+    z: (i) => 15 * i,
+    duration: 1,
+    ease: 'cubic',
+    stagger: -0.1,
   })
+}
+
+
+  useEffect(() => {
+    initializeAlbums()
+  }, [])
+  // useGSAP(() => {
+
+  //   gsap.to(album.current, {
+  //     rotation: '+=360',
+  //     duration: 3,
+
+  //     ease: 'none'
+  //   })
+  // })
 
   return (
     <div className="relative w-[100vw] h-[89vh] overflow-hidden bg-red-400">
       <div className="slider absolute top-[15vh] w-[100vw] h-[100vh] overflow-hidden bg-purple-500">
-        <div ref={album} className="album">
-          <img className="w-[100%] h-[100%] object-cover" src="/postmodern.jpg" alt="Postmodern Jukebox Cover" />
-          <div className="copy">
-            <h1>Lorem, ipsumn.</h1>
+        {albumRefs.map((ref, index) => (
+          <div ref={ref} className="album" key={v4()}>
+            <img className="w-[100%] h-[100%] object-cover" src={`/album${index}.jpg`} alt="Album Cover" />
+            <div className="copy">
+              <h1>Lorem, ipsumn.</h1>
+            </div>
           </div>
-        </div>
-        {/* <div className="album">
-          <img className="w-[100%] h-[100%] object-cover" src="/floyd.jpg" alt="Pink Floyd Cover" />
-          <div className="copy">
-            <h1>Lorem, ipsumn.</h1>
-          </div>
-        </div>
-        <div className="album">
-          <img className="w-[100%] h-[100%] object-cover" src="/jaya.jpg" alt="Jaya the Cat Cover" />
-          <div className="copy">
-            <h1>Lorem, ipsumn.</h1>
-          </div>
-        </div>
-        <div className="album">
-          <img className="w-[100%] h-[100%] object-cover" src="/zeppelin.jpg" alt="Led Zeppelin Cover" />
-          <div className="copy">
-            <h1>Lorem, ipsumn.</h1>
-          </div>
-        </div>
-        <div className="album">
-          <img className="w-[100%] h-[100%] object-cover" src="/stones.jpg" alt="Rolling Stones Cover" />
-          <div className="copy">
-            <h1>Lorem, ipsumn.</h1>
-          </div>
-        </div> */}
+        ))}
       </div>
     </div>
   )
