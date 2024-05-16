cd3e4643b427c1eeed9451e53a4ecf4cc3f3562e
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-05-16 20:36:46 +0100

    Spinning Vinyl - added

--- src/pages/VinylAlbum.tsx
+++ src/pages/VinylAlbum.tsx
@@ -1,14 +1,27 @@
 import { useParams } from "react-router-dom";
 import { SPOTIFY_PLAYLISTS } from "../common/constants";
+import gsap from 'gsap';
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
+gsap.registerPlugin(useGSAP);
 
 const VinylAlbum = () => {
 
   const { album } = useParams();
+  const vinylRef = useRef<HTMLImageElement | null>(null);
 
+  useGSAP(() => {
+    gsap.to(vinylRef.current, {
+      rotate: "+=360",
+      repeat: -1,
+      ease: "none",
+      duration: 5,
+    })
+  })
 
   return (
-    <>
+    <div className="overflow-y-hidden">
       {album &&
         <iframe
           className="absolute top-0 w-[28%] h-[89%] mt-24"
@@ -18,7 +31,10 @@ const VinylAlbum = () => {
           loading="lazy"
         />
       }
-    </>
+
+        <img ref={vinylRef} src="/vinyl.png" className=" spinning-vinyl w-128 h-128 right-0 absolute mr-80 mt-28" alt="Vinyl Disc Spinning" />
+    
+    </div>
   )
 };
 export default VinylAlbum;
