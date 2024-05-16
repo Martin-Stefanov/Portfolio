f18bd2a0bf91bcd7dd18f8c4384390b17a926c95
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-05-16 17:47:06 +0100

    SPOTIFY_PLAYLISTS constant - added

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,13 @@
 
-export const CONNECTIFY_VIDEO:string = '';
-export const TRAVELART_VIDEO:string = 'globe-spinning.mp4';
-export const GIFIT_VIDEO:string = 'gif-it-video.mp4';
+export const CONNECTIFY_VIDEO: string = '';
+export const TRAVELART_VIDEO: string = 'globe-spinning.mp4';
+export const GIFIT_VIDEO: string = 'gif-it-video.mp4';
+
+
+export const SPOTIFY_PLAYLISTS: Record<number, string>= {
+  0: '00gpn3YRmN5T7pYrQkUMs6',
+  1: '43TJjpNBfw0qY6E87VeIxX',
+  2: '1J8QW9qsMLx3staWaHpQmU',
+  3: '6khax1qrW3LNlfAxwgEBq4',
+  4: '7wzStEU2keGohEu8jpVMZW'
+};
