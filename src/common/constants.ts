cadf23c8ce8364a7ec8068c39ea21534a703ca50
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-11-19 18:44:48 +0000

    Skills page - design added

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,13 +1,50 @@
+export const CONNECTIFY_VIDEO: string = "";
+export const TRAVELART_VIDEO: string = "globe-spinning.mp4";
+export const GIFIT_VIDEO: string = "gif-it-video.mp4";
 
-export const CONNECTIFY_VIDEO: string = '';
-export const TRAVELART_VIDEO: string = 'globe-spinning.mp4';
-export const GIFIT_VIDEO: string = 'gif-it-video.mp4';
-
-
-export const SPOTIFY_PLAYLISTS: Record<number, string>= {
-  0: '00gpn3YRmN5T7pYrQkUMs6',
-  1: '43TJjpNBfw0qY6E87VeIxX',
-  2: '1J8QW9qsMLx3staWaHpQmU',
-  3: '6khax1qrW3LNlfAxwgEBq4',
-  4: '7wzStEU2keGohEu8jpVMZW'
+export const SPOTIFY_PLAYLISTS: Record<number, string> = {
+  0: "00gpn3YRmN5T7pYrQkUMs6",
+  1: "43TJjpNBfw0qY6E87VeIxX",
+  2: "1J8QW9qsMLx3staWaHpQmU",
+  3: "6khax1qrW3LNlfAxwgEBq4",
+  4: "7wzStEU2keGohEu8jpVMZW",
 };
+
+export const SKILLS_LOGOS: { name: string; url: string }[] = [
+  {
+    name: "JavaScript",
+    url: "Skills/js-logo.png",
+  },
+  {
+    name: "TypeScript",
+    url: "Skills/ts-logo.png",
+  },
+  {
+    name: "ReactJS",
+    url: "Skills/react-logo.png",
+  },
+  {
+    name: "NextJS",
+    url: "Skills/next-logo.png",
+  },
+  {
+    name: "Tailwind CSS",
+    url: "Skills/tailwind-logo.png",
+  },
+  {
+    name: "Prisma ORM",
+    url: "Skills/prisma-logo.png",
+  },
+  {
+    name: "HTML",
+    url: "Skills/html-logo.png",
+  },
+  {
+    name: "CSS",
+    url: "Skills/css-logo.png",
+  },
+  {
+    name: "Git",
+    url: "Skills/git-logo.png",
+  },
+];
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,48 +1,91 @@
 import { useEffect, useState } from "react";
-import { v4 } from "uuid";
+import { SKILLS_LOGOS } from "../common/constants";
 
 const Skills = () => {
-
-  const [logos, setLogos] = useState(['js-logo.jpg', 'ts-logo.png', 'html-logo.png', 'css-logo.png']);
-  const [displayedLogos, setDisplayedLogos] = useState<string[]>([]);
-
-  const delay = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration));
+  const [logosToShow, setLogosToShow] = useState<
+    { name: string; url: string }[]
+  >([]);
 
   useEffect(() => {
-    const displayLogos = async () => {
-      for (const logo of logos) {
-        if (logo !== logos[0]) {
-          await delay(2000);
-        }
-        setDisplayedLogos((prevLogos) => [...prevLogos, logo]);
+    let isMounted = true;
+
+    const showLogos = async () => {
+      for (const logo of SKILLS_LOGOS) {
+        if (!isMounted) return;
+        setLogosToShow((prev) => {
+          // Prevent duplicates by ensuring the logo isn't already in the state
+          if (!prev.includes(logo)) {
+            return [...prev, logo];
+          }
+          return prev;
+        });
+        await delay(1500); // Delay before showing the next logo
       }
     };
+    showLogos();
+    return () => {
+      isMounted = false; // Clean up to avoid multiple renders
+    };
+  }, []);
 
-    displayLogos();
-  }, [logos]);
-
-
-  const removeDuplicates = (logosArray: string[]): string[] => {
-    const logosSet = new Set(logosArray);
-    return [...logosSet];
-  }
+  const delay = (duration: number) =>
+    new Promise((resolve) => setTimeout(resolve, duration));
 
   return (
-    <div>
-      {Boolean(displayedLogos.length) && removeDuplicates(displayedLogos).map((logo) => (
-        <div className="inline-block" key={v4()}>
-          <img
-            key={logo}
-            className='single-logo'
-            src={logo}
-            alt="TEST"
-          />
-        </div>
-
-      ))}
+    <div className="h-full bg-gray-300 flex flex-col overflow-hidden">
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
+        {logosToShow.map((logo) => (
+          <div
+            key={logo.url}
+            className="flex flex-col items-center text-center"
+          >
+            <img className="single-logo" src={logo.url} alt={logo.name} />
+            <span className="font-bold text-md sm:text-xl md:text-2xl lg:text-3xl">
+              {logo.name}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
-
-
-}
+};
 export default Skills;
+
+// const [logos, setLogos] = useState(['js-logo.jpg', 'ts-logo.png', 'html-logo.png', 'css-logo.png']);
+// const [displayedLogos, setDisplayedLogos] = useState<string[]>([]);
+
+// const delay = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration));
+
+// useEffect(() => {
+//   const displayLogos = async () => {
+//     for (const logo of logos) {
+//       if (logo !== logos[0]) {
+//         await delay(2000);
+//       }
+//       setDisplayedLogos((prevLogos) => [...prevLogos, logo]);
+//     }
+//   };
+
+//   displayLogos();
+// }, [logos]);
+
+// const removeDuplicates = (logosArray: string[]): string[] => {
+//   const logosSet = new Set(logosArray);
+//   return [...logosSet];
+// }
+
+// return (
+//   <div>
+//     {Boolean(displayedLogos.length) && removeDuplicates(displayedLogos).map((logo) => (
+//       <div className="inline-block" key={v4()}>
+//         <img
+//           key={logo}
+//           className='single-logo'
+//           src={logo}
+//           alt="TEST"
+//         />
+//       </div>
+
+//     ))}
+//   </div>
+// );
