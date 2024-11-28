c926fae2e99666cc24ce88e8432bd79a1a520fd2
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-11-28 19:28:36 +0000

    NavBar - responsive design added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,19 +5,16 @@ const NavBar = () => {
   const navigate = useNavigate();
 
   return (
-    // <div className="h-24 w-full min-w-max bg-black top-0">
-      <div className="flex justify-between items-center h-auto px-2  bg-gray-800">
-        <img onClick={() => navigate('/')} className="h-10 sm:h-16 cursor-pointer" src="/logo.png" alt="MS-Logo" />
-        <div className="flex w-[55%] justify-between">
+      <div className="flex items-center h-auto px-2 bg-gray-800">
+        <img onClick={() => navigate('/')} className=" h-10 sm:h-14 md:h-16 cursor-pointer mr-auto" src="/logo.png" alt="MS-Logo" />
+        <div className="flex justify-center flex-grow gap-2 sm:gap-7 lg:gap-24 xl:gap-28 xl:mr-24">
           <button onClick={() => navigate('/projects')} className='main-button'>Projects</button>
           <button onClick={() => navigate('/skills')} className='main-button'>Skills</button>
           <button onClick={() => navigate('/hobbies')} className='main-button'>Hobbies</button>
           <button className='main-button'>Contacts</button>
         </div>
-        
-        <img className="h-7 w-7 sm:w-12 sm:h-12 mx-4 cursor-pointer active:w-14 active:h-14 transition duration-100 hover:opacity-50" src="/github.png" alt="GitHub-Logo" />
+        {/* <img className="h-7 w-7 sm:w-12 sm:h-12 mx-4 cursor-pointer active:w-14 active:h-14 transition duration-100 hover:opacity-50" src="/github.png" alt="GitHub-Logo" /> */}
       </div>
-    // </div>
   )
 
 }
