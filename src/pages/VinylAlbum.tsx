5b8dcce81544f714148055b8d90f743cccc8a089
Martin.Stefanov <martin.p.stefanov@gmail.com>
2024-11-29 15:48:35 +0000

    VinylAlbum page - record rotates only while music plays

--- src/pages/VinylAlbum.tsx
+++ src/pages/VinylAlbum.tsx
@@ -1,40 +1,84 @@
 import { useParams } from "react-router-dom";
 import { SPOTIFY_PLAYLISTS } from "../common/constants";
-import gsap from 'gsap';
+import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 gsap.registerPlugin(useGSAP);
 
 const VinylAlbum = () => {
-
   const { album } = useParams();
   const vinylRef = useRef<HTMLImageElement | null>(null);
+  const tonearmRef = useRef<HTMLImageElement | null>(null);
+  const [playing, setPlaying] = useState(false);
 
-  useGSAP(() => {
-    gsap.to(vinylRef.current, {
-      rotate: "+=360",
-      repeat: -1,
-      ease: "none",
-      duration: 5,
-    })
-  })
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.origin !== "https://open.spotify.com") return;
+
+      const data = event.data;
+      if (data.payload.isPaused) {
+        setPlaying(false);
+      } else {
+        setPlaying(true);
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (vinylRef.current) {
+      if (playing) {
+        gsap.to(vinylRef.current, {
+          rotate: "+=360",
+          repeat: -1,
+          ease: "none",
+          duration: 5,
+        });
+        // gsap.to(tonearmRef.current, {
+
+        // })
+      } else {
+        gsap.killTweensOf(vinylRef.current);
+        // gsap.to(tonearmRef.current, {
+
+        // })
+      }
+    }
+  }, [playing]);
+  console.log("RENDERED");
 
   return (
     <div className="flex flex-col items-center h-full overflow-hidden mt-10 sm:mt-0 sm:flex-row sm:justify-center">
-      {album &&
+      {album && (
         <iframe
           className="ml-4 sm:h-[38rem] sm:mr-6 md:mr-16 lg:h-[89%] lg:w-[28%] lg:min-w-80"
-          src={`https://open.spotify.com/embed/album/${SPOTIFY_PLAYLISTS[parseInt(album)]}?utm_source=generator&theme=0`}
+          src={`https://open.spotify.com/embed/album/${
+            SPOTIFY_PLAYLISTS[parseInt(album)]
+          }?utm_source=generator&theme=0`}
           frameBorder="0"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         />
-      }
-
-        <img ref={vinylRef} src="/vinyl.png" className="w-44 h-44 sm:w-72 sm:h-72 md:h-96 md:w-96 lg:w-[36rem] lg:h-[36rem]" alt="Vinyl Disc Spinning" />
-    
+      )}
+      <img
+        ref={vinylRef}
+        src="/vinyl.png"
+        className="w-44 h-44 sm:w-72 sm:h-72 md:h-96 md:w-96 lg:w-[36rem] lg:h-[36rem]"
+        alt="Vinyl Disc Spinning"
+      />
+      <img
+        ref={tonearmRef}
+        src="/Hobbies/tonearm1.png"
+        className="lg:h-128 top-0  right-32 top-32"
+        alt="Tonearm"
+      />
     </div>
-  )
+  );
 };
 export default VinylAlbum;
